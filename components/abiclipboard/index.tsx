ce75d9e98efc1daac424a742d030a00a83c96c4c
Paul Gadi <paulgadi@gmail.com>
2023-05-22 18:49:30 -0400

    add check image when copied to clipboard

--- components/abiclipboard/index.tsx
+++ components/abiclipboard/index.tsx
@@ -1,9 +1,10 @@
 import { MinusIcon, PlusIcon } from "@heroicons/react/24/outline";
-import { Square2StackIcon } from "@heroicons/react/24/outline";
+import { CheckIcon, Square2StackIcon } from "@heroicons/react/24/outline";
 import { useToggle } from "../../hooks";
 import { Anchor } from "../anchor";
 import contractData from "../../contractInfo"
 import _ from "lodash";
+import { useState } from "react";
 
 export const AbiClipboard = ({ abi, chain, address, name }) => {
 
@@ -11,6 +12,7 @@ export const AbiClipboard = ({ abi, chain, address, name }) => {
   const { state: collapsed, toggle: toggleCollapsed } =
   useToggle(true);
 
+  const [isCopied, setIsCopied] = useState(false);
  
 const newContractInfoJson = () => {
   const contractInfo = {
@@ -36,7 +38,14 @@ const newContractInfoJson = () => {
 }
 
   const handleCopyAddress = () => {
+    
     navigator.clipboard.writeText(newContractInfoJson());
+
+    setIsCopied(true);
+
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 2000);    
   };      
 
   const [CollapseIcon, collapseText] = collapsed
@@ -47,7 +56,7 @@ const newContractInfoJson = () => {
     <>
     <div className="flex items-center gap-2">
       <h4 className="">
-        ABI Found. 
+        <b>ABI Found.</b>
       </h4>
       {/*
       View here  &#8594;
@@ -68,14 +77,18 @@ const newContractInfoJson = () => {
         <Anchor href="https://github.com/polats/brevity/blob/main/contractInfo.ts">
             contractInfo.ts 
           </Anchor>
-          &#160;with the new ABI from this clipboard &#8594;
+          &#160;with the new ABIs. Copy to clipboard here &#8594;
       </h4>
       <button
             className="inline p-0.5 mx-0.5 rounded-sm text-black dark:text-white focus:bg-black focus:text-white dark:focus:bg-white dark:focus:text-black focus:outline-none"
             onClick={handleCopyAddress}
           >
             <span className="sr-only">Copy Address</span>
-            <Square2StackIcon className="h-6 w-6" />
+            {
+              isCopied ?
+              <CheckIcon className="h-6 w-6" />
+              : <Square2StackIcon className="h-6 w-6" />
+            }
           </button>      
       </div>
       
